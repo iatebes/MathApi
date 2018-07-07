677459e42fd19cb4ec1450429fda59a93bebe68d
Kin <clgt@outlook.com>
2018-07-08 01:52:27 +0700

    fix del container

--- del_container.sh
+++ del_container.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 container_name="$1"
-if [[ "$(docker ps | grep $container_name} 2> /dev/null)" != "" ]]; then
+if [[ "$(docker ps | grep $container_name 2> /dev/null)" != "" ]]; then
   echo "exists and running"
   docker stop $container_name
   docker rm $container_name
-elif [[ "$(docker ps -a | grep $container_name} 2> /dev/null)" != "" ]]; then
+elif [[ "$(docker ps -a | grep $container_name 2> /dev/null)" != "" ]]; then
   echo "exists but stopped"
   docker rm $container_name
 else
