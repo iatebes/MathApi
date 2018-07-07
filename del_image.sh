449124f07542d3b8f1404ab8effb5e4d6406c093
Kin <clgt@outlook.com>
2018-07-08 01:41:25 +0700

    fix del image

--- del_image.sh
+++ del_image.sh
@@ -3,7 +3,7 @@ image_and_tag="$1"
 image_and_tag_array=(${image_and_tag//:/ })
 if [[ "$(docker images ${image_and_tag_array[0]} | grep ${image_and_tag_array[1]} 2> /dev/null)" != "" ]]; then
   echo "exists"
-  docker rmi $image_and_tag
+  docker rmi -f $image_and_tag
 else
   echo "$image_and_tag doesn't exist"
 fi
