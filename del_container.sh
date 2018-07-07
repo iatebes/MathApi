552ea1a15678f9a8c3ce0c69683e66b03031395b
Kin <clgt@outlook.com>
2018-07-08 00:50:42 +0700

    add del shell

---
+++ del_container.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+container_name="$1"
+if [[ "$(docker ps | grep $container_name} 2> /dev/null)" != "" ]]; then
+  echo "exists and running"
+  docker stop $container_name
+  docker rm $container_name
+elif [[ "$(docker ps -a | grep $container_name} 2> /dev/null)" != "" ]]; then
+  echo "exists but stopped"
+  docker rm $container_name
+else
+  echo "$container_name doesn't exist"
+fi
---
+++ del_image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+image_and_tag="$1"
+image_and_tag_array=(${image_and_tag//:/ })
+if [[ "$(docker images ${image_and_tag_array[0]} | grep ${image_and_tag_array[1]} 2> /dev/null)" != "" ]]; then
+  echo "exists"
+  docker rmi $image_and_tag
+else
+  echo "$image_and_tag doesn't exist"
+fi
